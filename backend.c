97f7cd44f25431e939d10e3249f25db665cc9619
Ziyan Zhou <zhou@ziyan.info>
2012-11-15 21:01:42 -0800

    Make backend secure.

--- backend.c
+++ backend.c
@@ -19,16 +19,163 @@ static option_t options[] = {
 
 static int backend_check(void)
 {
+    if (backend)
+    {
+        return 1;
+    }
+    return 0;
+}
+
+static int backend_exec(const char *cmd, char *argv[], pid_t *ppid, FILE **pfp_read, FILE **pfp_write)
+{
+    int fd_read[2];
+    int fd_write[2];
+    pid_t pid = 0;
+    FILE *fp_read = NULL;
+    FILE *fp_write = NULL;
+
+    *ppid = 0;
+    *pfp_read = NULL;
+    *pfp_write = NULL;
+
+    //
+    // Create the pipes.
+    //
+
+    if (pipe(fd_read) < 0)
+    {
+        return 0;
+    }
+
+    if (pipe(fd_write) < 0)
+    {
+        close(fd_read[0]);
+        close(fd_read[1]);
+        return 0;
+    }
+
+    //
+    // Fork.
+    //
+
+    pid = fork();
+    if (pid < 0)
+    {
+        close(fd_read[0]);
+        close(fd_read[1]);
+        close(fd_write[0]);
+        close(fd_write[1]);
+        return 0;
+    }
+
+    //
+    // If we are child process.
+    //
+
+    if (pid == 0)
+    {
+        //
+        // Redirect pipes.
+        //
+
+        if (fd_read[1] != STDOUT_FILENO)
+        {
+            dup2(fd_read[1], STDOUT_FILENO);
+            close(fd_read[1]);
+        }
+        close(fd_read[0]);
+
+        if (fd_write[0] != STDIN_FILENO)
+        {
+            dup2(fd_write[0], STDIN_FILENO);
+            close(fd_write[0]);
+        }
+        close(fd_write[1]);
+
+        //
+        // Execute the backend with name as the argument.
+        //
+
+        execv(backend, argv);
+        _exit(127);
+        return 0;
+    }
+
+    //
+    // We are the parent process.
+    //
+
+    close(fd_read[1]);
+    close(fd_write[0]);
+
+    //
+    // Connect up read and write pipe to child process.
+    //
+
+    fp_read = fdopen(fd_read[0], "r");
+    if (fp_read == NULL)
+    {
+        close(fd_read[0]);
+        close(fd_write[1]);
+        return 0;
+    }
+
+    fp_write = fdopen(fd_write[1], "w");
+    if (fp_write == NULL)
+    {
+        fclose(fp_read);
+        close(fd_write[1]);
+        return 0;
+    }
+
+    //
+    // No failure.
+    //
+
+    *ppid = pid;
+    *pfp_read = fp_read;
+    *pfp_write = fp_write;
+
+    return 1;
+}
+
+static int backend_close(pid_t pid, FILE *fp_read, FILE *fp_write)
+{
+    int result_read = 0;
+    int result_write = 0;
+
+    //
+    // Close and wait till child process exits.
+    //
+
+    result_read = fclose(fp_read);
+    result_write = fclose(fp_write);
+
+    if (result_read == EOF || result_write == EOF)
+    {
+        return 0;
+    }
+
+    while (waitpid(pid, NULL, 0) < 0)
+    {
+        if (errno != EINTR)
+        {
+            return 0;
+        }
+    }
+
     return 1;
 }
 
 static int backend_verify(char *name, char *ourname, int id, struct chap_digest_type *digest, unsigned char *challenge, unsigned char *response, char *message, int message_space)
 {
     int ok = 0;
-    char command[512];
-    FILE *fp;
     unsigned char secret[MAXSECRETLEN + 1];
+    FILE *fp_read = NULL;
+    FILE *fp_write = NULL;
+    pid_t pid = 0;
     int secret_len = 0;
+    char *argv[3] = {backend, name, NULL};
 
     //
     // Need to have a backend specified.
@@ -40,28 +187,35 @@ static int backend_verify(char *name, char *ourname, int id, struct chap_digest_
     }
     
     //
-    // Construct the command to be issued.
+    // Execute backend.
     //
 
-    if (snprintf(command, sizeof(command), backend, name) <= 0)
-    {
-        return 0;
-    }
-    info("BACKEND executing: %s", command);
-    
-    fp = popen(command, "r");
-    if (fp == NULL)
+    if (!backend_exec(backend, argv, &pid, &fp_read, &fp_write))
     {
         return 0;
     }
 
-    if (fgets(secret, sizeof(secret), fp) != NULL)
+    info("BACKEND plugin executing: %s", backend);
+
+    //
+    // Read backend output.
+    //
+
+    if (fgets(secret, sizeof(secret), fp_read) != NULL)
     {
+        //
+        // Strip trailing return character.
+        //
+
         secret_len = strlen(secret);
-        while (secret_len > 0 && secret[secret_len - 1] == '\n')
+        if (secret_len > 0 && secret[secret_len - 1] == '\n')
         {
             secret_len--;
         }
+
+        //
+        // Verify challenge response.
+        //
 
         if (secret_len > 0)
         {
@@ -69,9 +223,13 @@ static int backend_verify(char *name, char *ourname, int id, struct chap_digest_
         }
     }
 
+    //
+    // Clean up.
+    //
+
     memset(secret, 0, sizeof(secret));
 
-    pclose(fp);
+    backend_close(pid, fp_read, fp_write);
 
     return ok;
 }
